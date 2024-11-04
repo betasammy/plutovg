32bbf9b1960038b8521f08eef053e30b6a383a29
betasammy <betacomputer2005@gmail.com>
2024-11-04 08:09:46 +0100

    Update smiley.c

--- examples/smiley.c
+++ examples/smiley.c
@@ -1,13 +1,83 @@
 #include <plutovg.h>
 
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+#if defined(_MSC_VER)
+#include <intrin.h>
+static inline int clz(unsigned int x) {
+    unsigned long r = 0;
+    if (_BitScanReverse(&r, x))
+        return 31 - r;
+    return 32;
+}
+#define PVG_FT_MSB(x)  (31 - clz(x))
+#elif defined(__GNUC__)
+#define PVG_FT_MSB(x)  (31 - __builtin_clz(x))
+#else
+static inline int clz(unsigned int x) {
+    for (int i = 31; i >= 0; i--)
+    {
+        if (x >> i)
+        {
+            return 31 - i;
+        }
+    }
+
+    return 32;
+}
+#define PVG_FT_MSB(x)  (31 - clz(x))
+#endif
+
 int main(void)
 {
+#if defined(_WIN32)
+    printf("_WIN32\n");
+#endif
+#if defined(_MSC_VER)
+    printf("_MSC_VER\n");
+#endif
+#if defined(__APPLE__)
+    printf("__APPLE__\n");
+#endif
+#if defined(__clang__)
+    printf("__clang__\n");
+#endif
+#if defined(__GNUC__)
+    printf("__GNUC__\n");
+#endif
+    char buffer[256];
+    FILE *fp;
+
+    // Open a pipe to run fc-list and read its output
+    fp = popen("fc-list", "r");
+    if (fp == NULL) {
+        perror("popen failed");
+        return 1;
+    }
+
+    // Read and print each line from fc-list
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+
+    // Close the pipe
+    pclose(fp);
+
+    return 0;
+    
+    printf("%d\n", PVG_FT_MSB(0));
+    printf("%d\n", PVG_FT_MSB(10));
+    printf("%d\n", PVG_FT_MSB(100));
+    printf("%d\n", PVG_FT_MSB(1000));
+    
     const int width = 150;
     const int height = 150;
 
     plutovg_surface_t* surface = plutovg_surface_create(width, height);
     plutovg_canvas_t* canvas = plutovg_canvas_create(surface);
-
+    
     float center_x = width / 2.f;
     float center_y = height / 2.f;
     float face_radius = 70;
@@ -19,7 +89,7 @@ int main(void)
     float eye_y = center_y - eye_offset_y;
 
     plutovg_canvas_save(canvas);
-    plutovg_canvas_arc(canvas, center_x, center_y, face_radius, 0, PLUTOVG_TWO_PI, 0);
+    plutovg_canvas_arc(canvas, center_x, center_y, face_radius, 0, 2 * PLUTOVG_PI, 0);
     plutovg_canvas_set_rgb(canvas, 1, 1, 0);
     plutovg_canvas_fill_preserve(canvas);
     plutovg_canvas_set_rgb(canvas, 0, 0, 0);
@@ -28,8 +98,8 @@ int main(void)
     plutovg_canvas_restore(canvas);
 
     plutovg_canvas_save(canvas);
-    plutovg_canvas_arc(canvas, eye_x, eye_y, eye_radius, 0, PLUTOVG_TWO_PI, 0);
-    plutovg_canvas_arc(canvas, center_x + eye_offset_x, eye_y, eye_radius, 0, PLUTOVG_TWO_PI, 0);
+    plutovg_canvas_arc(canvas, eye_x, eye_y, eye_radius, 0, 2 * PLUTOVG_PI, 0);
+    plutovg_canvas_arc(canvas, center_x + eye_offset_x, eye_y, eye_radius, 0, 2 * PLUTOVG_PI, 0);
     plutovg_canvas_set_rgb(canvas, 0, 0, 0);
     plutovg_canvas_fill(canvas);
     plutovg_canvas_restore(canvas);
